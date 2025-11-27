6b04cd09d4906c8afff9bcf9c90398d88e29cfed
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2025-11-28 02:21:07 +0900

    feat: 회원가입/로그인/로그아웃 api 작성

---
+++ src/apis/auth.ts
@@ -0,0 +1,35 @@
+import { apiClient } from "./apiClient";
+
+export const signup = async (username: string, password: string) => {
+  try {
+    const res = await apiClient.post("/auth/signup", {
+      username,
+      password,
+    });
+    return res.data;
+  } catch (error) {
+    console.error("회원가입에러: ", error);
+    throw error;
+  }
+};
+
+export const login = async (username: string, password: string) => {
+  try {
+    const res = await apiClient.post("/auth/login", {
+      username,
+      password,
+    });
+    return res.data;
+  } catch (error) {
+    console.error("로그인에러: ", error);
+    throw error;
+  }
+};
+
+export const logout = async () => {
+  try {
+    const res = await apiClient.post("/auth/logout", {});
+  } catch (error) {
+    console.log("로그아웃에러: ", error);
+  }
+};
